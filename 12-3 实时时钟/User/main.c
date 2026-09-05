4e6673cd17a8853482fe837f4539cb2dfbdcb6f4
IMcribs <yixi_qwer@163.com>
2026-09-05 12:23:55 +0800

    12 - 3 实时时钟
    结构体 指针在理解一下

---
+++ 12-3 实时时钟/User/main.c
@@ -0,0 +1,32 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "OLED.h"
+#include "MYRTC.h"
+
+int main(void)
+{
+	
+	OLED_Init();
+	My_RTCInit();
+	
+	OLED_ShowString(1,1,"Data:XXXX-XX-XX");
+	OLED_ShowString(2,1,"Time:XX:XX:XX");
+	OLED_ShowString(3,1,"CNT:");
+	OLED_ShowString(4,1,"DIV:");
+	
+	
+	while (1)
+	{
+		MyRTC_ReadTime();
+		
+		OLED_ShowNum(1, 6, MyRTC_Time[0], 4);
+		OLED_ShowNum(1, 11, MyRTC_Time[1], 2);
+		OLED_ShowNum(1, 14, MyRTC_Time[2], 2);
+		OLED_ShowNum(2, 6, MyRTC_Time[3], 2);
+		OLED_ShowNum(2, 9, MyRTC_Time[4], 2);
+		OLED_ShowNum(2, 12, MyRTC_Time[5], 2);
+		
+		OLED_ShowNum(3, 6, RTC_GetCounter(), 10);
+		OLED_ShowNum(4, 6, (32768 - RTC_GetDivider()) / 32768.0 * 999, 10);
+	}
+}
