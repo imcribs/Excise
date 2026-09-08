b300d1a24e8d772b808a4b5a93b49a08db1d3e1d
IMcribs <yixi_qwer@163.com>
2026-09-08 16:58:34 +0800

    13 - 1 低功耗模式
    低功耗模式 电源管理

---
+++ 13 -2 睡眠模式+串口发送+接收/Hardware/Serial.c
@@ -0,0 +1,138 @@
+#include "stm32f10x.h"                  // Device header
+#include "stdio.h"
+#include "stdarg.h"
+
+uint8_t Serial_RXData;
+uint8_t Serial_RXFlag;
+
+void Serial_Init(void)
+{
+	
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);
+	
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
+	
+	
+
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Mode= GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Pin= GPIO_Pin_9;
+	GPIO_InitStructure.GPIO_Speed= GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA,&GPIO_InitStructure);
+	
+	
+	GPIO_InitStructure.GPIO_Mode= GPIO_Mode_IPU;
+	GPIO_InitStructure.GPIO_Pin= GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Speed= GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA,&GPIO_InitStructure);
+	
+
+	USART_InitTypeDef  USART_InitStructure;
+	USART_InitStructure.USART_BaudRate = 9600 ;
+	USART_InitStructure.USART_HardwareFlowControl= USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode= USART_Mode_Tx | USART_Mode_Rx;
+	USART_InitStructure.USART_Parity=USART_Parity_No ;
+	USART_InitStructure.USART_StopBits= USART_StopBits_1;
+	USART_InitStructure.USART_WordLength= USART_WordLength_8b;
+	USART_Init(USART1,&USART_InitStructure);
+	
+	//寮�鍚痷sart涓柇
+	USART_ITConfig(USART1,USART_IT_RXNE,ENABLE);
+	
+	//閰嶇疆NVIC
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+	NVIC_InitTypeDef NVIC_InitStructure;
+	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1 ;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+	NVIC_Init(&NVIC_InitStructure);
+	
+	USART_Cmd(USART1,ENABLE);
+}
+
+void Serial_SendByte(uint8_t Byte)
+{
+
+	USART_SendData(USART1,Byte);
+	while(USART_GetFlagStatus(USART1,USART_FLAG_TXE) == RESET);
+
+}
+
+void Serial_SendArray(uint16_t *Array,uint16_t Length)  //不拷贝数据，而只是传递一个地址，节省内存,防止栈溢出
+{
+	uint16_t i;
+	for(i = 0;i < Length;i++)
+	{
+	Serial_SendByte(Array[i]);
+	}
+		
+}
+
+void Serial_SendString(char *String) //底层本质，字符串在c语言中本身就是指针
+	                                   // 同理不用传额外长度，只传字节地址，节省栈内存
+{
+	uint8_t i;
+	for(i = 0;String[i] != '\0';i++)  //字符串以\0为结尾，只需判断是否为0，从而判断字符串的大小
+	{
+	Serial_SendByte(String[i]);
+	}
+}
+
+uint32_t Serial_Pow(uint32_t X,uint32_t Y)
+{
+	uint32_t Result = 1;
+	while(Y--)
+	{
+	Result *= X;
+	}
+	return Result;
+
+}
+
+void Serial_SendNumber(uint32_t Number, uint8_t Length)
+{
+	uint8_t i;
+	for(i = 0; i < Length ; i++)
+	{
+	Serial_SendByte(Number / Serial_Pow(10,Length - i - 1) % 10 + '0');
+	}
+}
+
+int fputc(int ch, FILE *f)
+{
+	Serial_SendByte(ch);
+	return ch;
+}
+void Serial_printf(char *format,...)
+{
+	char String[100];
+	va_list arg;
+	va_start(arg,format);
+	vsprintf(String,format,arg);
+	va_end(arg);
+	Serial_SendString(String);
+}
+uint8_t Serial_GetRXFlag(void)
+{
+	if(Serial_RXFlag == 1)
+	{
+	Serial_RXFlag = 0;
+		return 1;
+	}
+	return 0;
+}
+uint8_t Serial_GetRXData(void)
+{
+	return Serial_RXData;
+}
+
+void USART1_IRQHandler(void)
+{
+	if(USART_GetITStatus(USART1,USART_IT_RXNE) == SET)
+	{
+	Serial_RXData = USART_ReceiveData(USART1);
+	Serial_RXFlag = 1;
+	USART_ClearITPendingBit(USART1,USART_IT_RXNE);
+	}
+}
